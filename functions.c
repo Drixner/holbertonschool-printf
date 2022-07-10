d710489ebec2751300b68f9075bb74bc719d8b70
Drixner <drixnerrafael1@gmail.com>
2022-07-10 15:45:59 -0500

    functions char and str

--- functions.c
+++ functions.c
@@ -13,3 +13,24 @@ int print_char(va_list args)
 	_putchar(va_arg(args, int));
 	return (1);
 }
+
+/**
+ *print_str - prints string
+ *@args: arguments
+ *Return: string
+ */
+
+int print_str(va_list args)
+{
+
+	int x;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+		str = "(null)";
+	for (x = 0; str[x]; x++)
+	{
+		_putchar(str[x]);
+	}
+	return (x);
+}
