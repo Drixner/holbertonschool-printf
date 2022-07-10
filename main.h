342189b9c1aa3671d95bcb58ad7a9de157b39c75
JulianZea <cesarjulianzeabravo@gmail.com>
2022-07-10 13:50:00 -0500

    library, structure and prototypes

--- main.h
+++ main.h
@@ -16,3 +16,13 @@ typedef struct func_type
 	char *t;
 	int (*f)(va_list);
 } func_t;
+
+int (*get_func(const char *format))(va_list);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_str(va_list args);
+int print_char(va_list args);
+int print_pct(va_list args);
+/*int print_dec(va_list args);*/
+
+#endif
